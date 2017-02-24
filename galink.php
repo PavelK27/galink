6f39379a29427ff8575a26c4923bb24504de3689
Pavel Korotenko <pavel@webdevstudios.com>
2017-02-24 10:43:41 -0500

    Code clean-up and sanitization

--- galink.php
+++ galink.php
@@ -1,57 +1,82 @@
 <?php
 /**
- * @package GA Link Button for WYSIWYG editor
- * @version 1.0
+ * Plugin Name: Google Analytics Link Tracking for WYSIWYG
+ * Plugin URI: http://www.github.com/PavelK27/galink
+ * Description: The simple plugin for adding Google Analytics Event tracking links via default WordPress WYSIWYG editor - TinyMCE. Please note, that you must have Google Analytics API (analytics.js) installed on your website in order for these links to work correctly.
+ * Author: Pavel Korotenko
+ * Version: 1.0
+ * Author URI: http://www.github.com/PavelK27
+ *
+ * @package Google Analytics Link Tracking for WYSIWYG
  */
-/*
-Plugin Name: Google Analytics Link Tracking for WYSIWYG
-Plugin URI: http://www.github.com/PavelK27/galink
-Description: The simple plugin for adding Google Analytics Event tracking links via default WordPress WYSIWYG editor - TinyMCE. Please note, that you must have Google Analytics API (analytics.js) installed on your website in order for these links to work correctly.
-Author: Pavel Korotenko
-Version: 1.0
-Author URI: http://www.github.com/PavelK27
-*/
-add_action('admin_head', 'galink_add_button');
 
+/**
+ * Add Hooks for GA Link button if we're in the editor
+ */
 function galink_add_button() {
-    global $typenow;
-    // check user permissions
-    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
-    return;
-    }
-    // verify the post type
-    if( ! in_array( $typenow, array( 'post', 'page' ) ) )
-        return;
-    // check if WYSIWYG is enabled
-    if ( get_user_option('rich_editing') == 'true') {
-        add_filter("mce_external_plugins", "galink_add_tinymce_plugin");
-        add_filter('mce_buttons', 'galink_register_button');
-    }
+	global $typenow;
+	// check user permissions.
+	if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+		return;
+	}
+	// verify the post type.
+	if ( ! in_array( $typenow, array( 'post', 'page' ), true ) ) {
+		return;
+	}
+	// check if WYSIWYG is enabled.
+	if ( get_user_option( 'rich_editing' ) === 'true' ) {
+	    add_filter( 'mce_external_plugins', 'galink_add_tinymce_plugin' );
+	    add_filter( 'mce_buttons', 'galink_register_button' );
+	}
+}
+add_action( 'admin_head', 'galink_add_button' );
+
+/**
+ * Adds js code for this plugin.
+ *
+ * @param array $plugin_array All TinyMCE Plugins.
+ */
+function galink_add_tinymce_plugin( $plugin_array ) {
+	$plugin_array['galink_button'] = plugins_url( '/text-button.js', __FILE__ );
+	return $plugin_array;
 }
 
-function galink_add_tinymce_plugin($plugin_array) {
-    $plugin_array['galink_button'] = plugins_url( '/text-button.js', __FILE__ ); 
-    return $plugin_array;
+/**
+ * Adds button for this plugin.
+ *
+ * @param array $buttons All TinyMCE Buttons.
+ */
+function galink_register_button( $buttons ) {
+	array_push( $buttons, 'galink_button' );
+	return $buttons;
 }
 
-function galink_register_button($buttons) {
-   array_push($buttons, "galink_button");
-   return $buttons;
-}
-
+/**
+ * Adds css for this plugin.
+ */
 function galink_css() {
-    wp_enqueue_style('galink', plugins_url('/style.css', __FILE__));
+	wp_enqueue_style( 'galink', plugins_url( '/style.css', __FILE__ ) );
 }
- 
-add_action('admin_enqueue_scripts', 'galink_css');
+add_action( 'admin_enqueue_scripts', 'galink_css' );
 
+/**
+ * Processes shortcode.
+ *
+ * @param array $atts shortcode attributes.
+ */
+function galink_process_shortcode( $atts ) {
+	// Sanitize shortcode attributes.
+	foreach ( $atts as $key => $value ) {
+		$atts[ $key ] = sanitize_text_field( $value );
+	}
+
+	// Set link target.
+	if ( 'true' === $atts['blank'] ) {
+	    $target = 'target="_blank" ';
+	} else {
+	    $target = '';
+	}
+
+	return '<a href="' . $atts['url'] . '" ' . $target . 'onclick="ga(\'send\', \'event\', \'' . $atts['ecat'] . '\', \'' . $atts['eaction'] . '\', \'' . $atts['elabel'] . '\', \'' . $atts['evalue'] . '\');">' . $atts['title'] . '</a>';
+}
 add_shortcode( 'galink', 'galink_process_shortcode' );
-
-function galink_process_shortcode($atts) {
-    if ($atts['blank'] == 'true') {
-        $target = 'target="_blank" ';
-    } else {
-        $target = '';
-    }
-    return '<a href="' . $atts['url'] . '" '. $target . 'onclick="ga(\'send\', \'event\', \'' . $atts['ecat'] . '\', \'' . $atts['eaction'] . '\', \''. $atts['elabel'] . '\', \'' . $atts['evalue'] . '\');">' . $atts['title'] . '</a>';
-}
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: pavelk27
+Donate link: http://github.com/PavelK27
+Tags: google analytics, link tracking
+Requires at least: 4.0.1
+Tested up to: 4.7.2
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The simple plugin for adding Google Analytics Event tracking links via default WordPress WYSIWYG editor - TinyMCE.
+
+== Description ==
+
+The simple plugin for adding Google Analytics Event tracking links via default WordPress WYSIWYG editor - TinyMCE. Please note, that you must have Google Analytics API (analytics.js) installed on your website in order for these links to work correctly. The plugin adds a shortcode button into the page editor and processes it when the page loads.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/ga-link-tracking-for-wysiwyg` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Use the WYSIWYG editor for adding shortcodes to posts/pages via the new button with Google Analytics logo.
+
+
+== Frequently Asked Questions ==
+
+= I'm getting 'ga is not defined' error in console =
+Please check if there's analytics.js: https://developers.google.com/analytics/devguides/collection/analyticsjs/ installed on your website.
+
+= It's still not working! =
+Try to install a browser extension for GA tags testing, as "Google Analytics Debugger" for example, to see if there are any errors with your codes. Also, keep in mind that you'll need to wait at least 24 hours, before events data will appear in reports. Though you can check Real-Time data in Real-Time\Events section of your GA panel.
+
+== Screenshots ==
+
+1. A new button which the plugin adds
+2. A pop-up window with link options
+3. A shortcode added into content area
+4. Processed shortcode in page content
+5. Google Analytics screenshot with Real-Time events data
+
+== Changelog ==
+= 1.0.1 =
+Code clean-up and sanitization.
+
+= 1.0 =
+First version with basic functionality
+
+== Upgrade Notice ==
+No upgrades so far!
